a1e2240fd6a362bb2799e0dea08444fee6896490
dilmurodd <axmad.sanaqulov@users.noreply.github.com>
2020-02-25 14:06:32 +0500

    25.02

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        navConteiner: true,
+        responsive:{
+            0:{
+                items:1,
+                dots: true,
+            },
+            768:{
+                items:2
+            },
+            1024:{
+                items:3
+            },
+            1336:{
+                items:4
+            }
+        }
+    });
+  });
+
+
+
+
+
+
 menu.onclick = function myFunction() {
     var x = document.getElementById("myMain-nav");
     if(x.className === "main-nav") {
